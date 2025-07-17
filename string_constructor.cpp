18038d5a3ca7960e4a2fa40d11488202579fbe6d
NureAlam68 <nurealam151068@gmail.com>
2025-07-17 20:03:16 +0600

    string constructor

---
+++ string_constructor.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // string s = "Hello";
+    // 1
+    // string s("Hello, World!");
+    // 2
+    // string s("Hello World", 5);
+    // 3
+    // string s = "Hello World";
+    // string t(s, 5);
+    // cout << t << endl;
+    // 4
+    string s(10, 'N');
+    cout << s << endl;
+    return 0;
+}
